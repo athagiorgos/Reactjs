aac4b059e857d44b1a4d115cec7988f465bacd6c
athagiorgos <athan711@gmail.com>
2020-12-10 15:00:26 +0200

    Fetching from apis

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -376,38 +376,74 @@
 
 
 
+// import React from "react"
+// //import Conditional from "./Conditional"
+
+// class App extends React.Component {
+
+//   constructor() {
+//     super()
+//     this.state = {
+//       loading: false,
+//       character: {}
+//     }
+//   }
+
+//   componentDidMount() {
+//     this.setState({
+//       loading: true
+//     })
+//     fetch("https://swapi.dev/api/people/1")
+//       .then(response => response.json())
+//       .then(data => {
+//         this.setState({
+//           loading: false,
+//           character: data
+//         })
+//       })
+//   }
+
+//   render() {
+//     return ( 
+//       <div>
+//         <h2>{this.state.loading ? "Loading..." : this.state.character.name}</h2>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
+
+
+
+
+
+
 import React from "react"
-//import Conditional from "./Conditional"
 
 class App extends React.Component {
-
   constructor() {
     super()
     this.state = {
-      loading: false,
-      character: {}
+      firstName: ""
     }
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  componentDidMount() {
+  handleChange(event) {
+    const {name, value} = event.target
     this.setState({
-      loading: true
+      [name]: value
     })
-    fetch("https://swapi.dev/api/people/1")
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          loading: false,
-          character: data
-        })
-      })
   }
 
   render() {
-    return ( 
-      <div>
-        <h2>{this.state.loading ? "Loading..." : this.state.character.name}</h2>
-      </div>
+    return (
+      <form>
+        <input type="text" value={this.state.firstName} name="firstName" placeholder="first Name" onChange={this.handleChange} />
+        <h1>{this.state.firstName}</h1>
+      </form>
     )
   }
 }
